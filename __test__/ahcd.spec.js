b8e184e16e3ee6b51f6077b2f210b4653e6c238e
Fumikazu Fujiwara <github@ze.gs>
2020-09-13 11:15:29 +0900

    0.0.1

--- __test__/ahcd.spec.js
+++
@@ -1,9 +0,0 @@
-const AppleHealthCareData = require('../src/ahcd');
-
-describe('AppleHealthCareData', () => {
-  it(' constructor() : can create new instance', () => {
-    const r = new AppleHealthCareData();
-    expect(r).not.toBeNull();
-    expect(r).toBeInstanceOf(AppleHealthCareData);
-  });
-});
