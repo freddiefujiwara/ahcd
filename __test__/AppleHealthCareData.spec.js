e8745d7db1a13832e23b848cbfa668b465460d54
Fumikazu Fujiwara <github@ze.gs>
2020-09-13 18:41:26 +0900

    add beforeEach

--- __test__/AppleHealthCareData.spec.js
+++ __test__/AppleHealthCareData.spec.js
@@ -1,26 +1,24 @@
 const AppleHealthCareData = require('../src/AppleHealthCareData');
 
 describe('AppleHealthCareData', () => {
+  let ahcd = undefined;
+  beforeEach(() =>{
+    ahcd = new AppleHealthCareData(require('fs').readFileSync('__test__/export.xml','utf-8'));
+  });
   it(' constructor() : can create new instance', () => {
-    const r = new AppleHealthCareData(require('fs').readFileSync('__test__/export.xml','utf-8'));
-    expect(r).not.toBeNull();
-    expect(r).toBeInstanceOf(AppleHealthCareData);
+    expect(ahcd).not.toBeNull();
+    expect(ahcd).toBeInstanceOf(AppleHealthCareData);
   });
   it(' analyze() : can output for prompt', () => {
-    let buffer = [];
-    const r = new AppleHealthCareData(require('fs').readFileSync('__test__/export.xml','utf-8'));
-    expect(r.analyze).toBeInstanceOf(Function);
+    expect(ahcd.analyze).toBeInstanceOf(Function);
   });
   it(' writeCsvs() : can be a function', () => {
-    const r = new AppleHealthCareData(require('fs').readFileSync('__test__/export.xml','utf-8'));
-    expect(r.writeCsvs).toBeInstanceOf(Function);
+    expect(ahcd.writeCsvs).toBeInstanceOf(Function);
   });
   it(' csv() : can be a function', () => {
-    const r = new AppleHealthCareData(require('fs').readFileSync('__test__/export.xml','utf-8'));
-    expect(r.csv).toBeInstanceOf(Function);
+    expect(ahcd.csv).toBeInstanceOf(Function);
   });
   it(' keys() : can be a function', () => {
-    const r = new AppleHealthCareData(require('fs').readFileSync('__test__/export.xml','utf-8'));
-    expect(r.keys).toBeInstanceOf(Function);
+    expect(ahcd.keys).toBeInstanceOf(Function);
   });
 });
