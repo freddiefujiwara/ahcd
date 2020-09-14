a3063ae39d5081d5c6e70c81683a99129da4cfd8
Fumikazu Fujiwara | Freddie <github@ze.gs>
2020-09-14 16:31:00 +0900

    Update form.js

--- docs/form.js
+++ docs/form.js
@@ -1,23 +1,18 @@
 $(function() {
   const ahcds = [];
   function handleFileSelect(evt) {
-    console.log(evt.target.file);
-    console.log(evt.target.files);
-    /*
-    evt.target.files.forEach((f) => {
-      const reader = new FileReader();
-      reader.onload = ((reader) => {
-        return () => {
-          const contents = reader.result;
-          const ahcd = new AppleHealthCareData(contents);
-          ahcd.analyze().writeCsvs();
-          ahcds.push(ahcd);
-          console.log(contents);
-        }
-      })(reader);
-      reader.readAsText(f);
-    });
-  */
+    const f = evt.target.files[0]
+    const reader = new FileReader();
+    reader.onload = ((reader) => {
+      return () => {
+        const contents = reader.result;
+        const ahcd = new AppleHealthCareData(contents);
+        ahcd.analyze().writeCsvs();
+        ahcds.push(ahcd);
+        console.log(contents);
+      }
+    })(reader);
+    reader.readAsText(f);
   }
   if (window.File && window.FileReader && window.FileList && window.Blob) {
     $('#files').on('change', handleFileSelect);
